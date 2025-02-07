43cdda62009c0716192bf537fa6acf9e19713410
Naresh Gupta <nareshgupta0899@gmail.com>
2025-02-07 16:05:00 +0530

    implemen chats

---
+++ lib/model/Chats.dart
@@ -0,0 +1,151 @@
+class Chat {
+  final String name;
+  final String? avatar;
+  final List<String>? messages;
+  Chat({required this.name, this.avatar, this.messages});
+
+  String getName() => name;
+  String getAvatar() => avatar ?? "";
+  List<String> getmessages() => messages ?? [];
+}
+
+final chats = [
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+  Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"), Chat(
+      name: "Naresh",
+      messages: [
+        "Hi",
+        "Hello"
+      ],
+      avatar: "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww"),
+];
--- lib/view/chats_view.dart
+++ lib/view/chats_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:menu_tab/model/Chats.dart';
 import 'package:menu_tab/view/custom_text_field.dart';
 
 class ChatsView extends StatefulWidget {
@@ -7,10 +8,6 @@ class ChatsView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ChatsState();
 }
-
-const List<Record> users=[
-  ()
-]
 
 class ChatsState extends State<ChatsView> {
   @override
@@ -23,9 +20,20 @@ class ChatsState extends State<ChatsView> {
             prefixIcon: Icons.search,
             suffixIconButton: Icons.camera,
           ),
-          ListView.builder(itemBuilder: (context, index){
-
-          })
+          ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Image.network(
+                  "https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww",
+                ),
+                title: Text(chats[index].name),
+                subtitle: Text(chats[index].name),
+                trailing: Text("1:40pm"),
+              );
+            },
+            itemCount: chats.length,
+          )
         ],
       ),
     );
